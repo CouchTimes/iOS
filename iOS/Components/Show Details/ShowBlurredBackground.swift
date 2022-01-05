968609fee8e979788802d5d0d733f68a133d2c2a
Jan Früchtl <jan@fruechtl.me>
2022-01-05 09:52:46 +0100

    Add stretch background support for show details

--- iOS/Components/Show Details/ShowBlurredBackground.swift
+++ iOS/Components/Show Details/ShowBlurredBackground.swift
@@ -11,28 +11,44 @@ import SwiftUI
 struct ShowBlurredBackground: View {
     var poster: Image
 
+    @State private var offset: CGFloat = 0
     @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        ZStack {
-            GeometryReader { geometry in
-                poster
-                    .resizable()
-                    .frame(width: geometry.size.width, height: UIScreen.main.bounds.size.height)
-                    .blur(radius: 16.0, opaque: true)
-
-                VStack(spacing: 0) {
-                    Rectangle()
-                        .fill(colorScheme == .dark ?
-                            LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.08), Color.black.opacity(0.32), Color.black.opacity(1)]), startPoint: .top, endPoint: .bottom) :
-                            LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.08), Color.white.opacity(0.32), Color.white.opacity(1)]), startPoint: .top, endPoint: .bottom)
-                        )
-                        .frame(width: geometry.size.width, height: UIScreen.main.bounds.size.height / 1.65)
-
-                    Rectangle()
-                        .fill(Color("backgroundColor"))
-                        .frame(width: geometry.size.width, height: UIScreen.main.bounds.size.height / 1.65)
+        let backgroundHeight = UIScreen.main.bounds.size.height
+        
+        return VStack {
+            GeometryReader { geometry -> AnyView in
+                let minY = geometry.frame(in: .global).minY
+                
+                DispatchQueue.main.async {
+                    self.offset = minY
+                    print(self.offset)
                 }
+                
+                return AnyView (
+                    ZStack(alignment: .top) {
+                        poster
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: geometry.size.width, height: minY > 0 ? backgroundHeight + minY : backgroundHeight, alignment: .center)
+                            .blur(radius: 16.0, opaque: true)
+
+                        VStack(spacing: 0) {
+                            Rectangle()
+                                .fill(colorScheme == .dark ?
+                                    LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.08), Color.black.opacity(0.32), Color.black.opacity(1)]), startPoint: .top, endPoint: .bottom) :
+                                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.08), Color.white.opacity(0.32), Color.white.opacity(1)]), startPoint: .top, endPoint: .bottom)
+                                )
+                                .frame(width: geometry.size.width, height: backgroundHeight)
+
+                            Rectangle()
+                                .fill(Color("backgroundColor"))
+                                .frame(width: geometry.size.width, height: backgroundHeight)
+                        }
+                    }
+                    .frame(height: minY > 0 ? backgroundHeight + minY : backgroundHeight, alignment: .center)
+                )
             }
         }
     }
